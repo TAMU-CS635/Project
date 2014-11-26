c6c5416c213a0f92eb31c458df03a0bdc4ce38d2
John Phillips <jphillips@localhost.localdomain>
2014-11-26 12:34:26 -0600

    added simplecv vision test

---
+++ vision.py
@@ -0,0 +1,22 @@
+from SimpleCV import Camera, Color
+def main():
+	cam = Camera()
+
+	while True:
+		image = cam.getImage()
+		blobs = image.findBlobs()
+		if blobs:
+			blobs.draw()
+			blob = find_yellow(blobs)
+			if blob:
+				image.drawCircle((blob.x, blob.y), 10, color=Color.RED)
+		image.show()
+
+def find_yellow(blobs):
+	for blob in blobs:
+		rgb = blob.meanColor()
+		if rgb[-1] < 50:
+			return blob
+	return None
+
+main()
