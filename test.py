31fef4c3a7212df7bc9c4d15a56648782b7f23bd
John Phillips <johnphillips@arch.tamu.edu>
2014-12-06 14:15:53 -0600

    sleep

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from create2 import Create
 from SimpleCV import Camera, Color
-
+import time
 
 robot = Create('/dev/ttyUSB0')
 
@@ -19,13 +19,17 @@ while True:
     blobs = dist.findBlobs()
     if blobs:
         blobs.draw()
-	image.show()
+	img.show()
 	blob = blobs[-1]
 	print "largest Blob area %s x %s" % (blob.area(), blob.x)
 	if blob.area() > 1000 and blob.x > 280 and blob.x < 360:
 		robot.go(20, 0)
+		time.sleep(1)
+		robot.stop()
 	else:
 		robot.go(0, 20)
+		time.sleep(1)
+		robot.stop()
         
 #result = img - dist
 #result.show()
